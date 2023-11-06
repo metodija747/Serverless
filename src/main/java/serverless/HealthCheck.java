e379bb5290affc38de76ed137f71066ac071df95
Metodija747 <metodijabucevski@yahoo.com>
2023-11-06 18:53:10 +0100

    added healthchecks

--- src/main/java/serverless/HealthCheck.java
+++ src/main/java/serverless/HealthCheck.java
@@ -27,44 +27,44 @@ public class HealthCheck implements RequestHandler<Map<String, Object>, Map<Stri
 
     @Override
     public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
-        Map<String, Object> dbStatus = checkDatabases();
+//        Map<String, Object> dbStatus = checkDatabases();
         Map<String, Object> httpStatus = checkHttpResources();
-        Map<String, Object> metricsStatus = checkMetrics();
+//        Map<String, Object> metricsStatus = checkMetrics();
         Map<String, Object> combinedStatus = new HashMap<>();
-        combinedStatus.putAll(dbStatus);
+//        combinedStatus.putAll(dbStatus);
         combinedStatus.putAll(httpStatus);
-        combinedStatus.putAll(metricsStatus);
+//        combinedStatus.putAll(metricsStatus);
         Map<String, Object> response = new HashMap<>();
         response.put("statusCode", 200);
         response.put("body", gson.toJson(combinedStatus));
         return response;
     }
 
-    public Map<String, Object> checkDatabases() {
-        String REGION = configManager.get("DYNAMO_REGION");
-        JsonObject configJson = gson.fromJson(configManager.get("HEALTH"), JsonObject.class);
-        var datasources = configJson.getAsJsonArray("DATASOURCES");
-
-        DynamoDbClient dynamoDB = DynamoDbClient.builder()
-                .region(Region.of(REGION))
-                .build();
-
-        ListTablesResponse listTablesResponse = dynamoDB.listTables();
-        List<String> availableTables = listTablesResponse.tableNames();
-
-        Map<String, Object> response = new HashMap<>();
-
-        for (var datasourceElement : datasources) {
-            String tableName = datasourceElement.getAsString();
-            if (availableTables.contains(tableName)) {
-                response.put(tableName, "UP");
-            } else {
-                response.put(tableName, "DOWN");
-            }
-        }
-
-        return response;
-    }
+//    public Map<String, Object> checkDatabases() {
+//        String REGION = configManager.get("DYNAMO_REGION");
+//        JsonObject configJson = gson.fromJson(configManager.get("HEALTH"), JsonObject.class);
+//        var datasources = configJson.getAsJsonArray("DATASOURCES");
+//
+//        DynamoDbClient dynamoDB = DynamoDbClient.builder()
+//                .region(Region.of(REGION))
+//                .build();
+//
+//        ListTablesResponse listTablesResponse = dynamoDB.listTables();
+//        List<String> availableTables = listTablesResponse.tableNames();
+//
+//        Map<String, Object> response = new HashMap<>();
+//
+//        for (var datasourceElement : datasources) {
+//            String tableName = datasourceElement.getAsString();
+//            if (availableTables.contains(tableName)) {
+//                response.put(tableName, "UP");
+//            } else {
+//                response.put(tableName, "DOWN");
+//            }
+//        }
+//
+//        return response;
+//    }
 
     public Map<String, Object> checkHttpResources() {
         JsonObject configJson = gson.fromJson(configManager.get("HEALTH"), JsonObject.class);
@@ -94,75 +94,75 @@ public class HealthCheck implements RequestHandler<Map<String, Object>, Map<Stri
         return httpChecks;
     }
 
-    public Map<String, Object> checkMetrics() {
-        JsonObject configJson = gson.fromJson(configManager.get("HEALTH"), JsonObject.class);
-        JsonObject metricsJson = configJson.getAsJsonObject("METRICS");
-        String timeRange = metricsJson.get("TIME_RANGE").getAsString();
-        long startTime = Instant.now().minusMillis(Long.parseLong(timeRange)).toEpochMilli();
-
-        CloudWatchClient cw = CloudWatchClient.builder().build();
-
-        Map<String, Object> metricsResponse = new HashMap<>();
-        for (JsonElement functionElement : metricsJson.getAsJsonArray("FUNCTIONS")) {
-            JsonObject functionObj = functionElement.getAsJsonObject();
-            String functionName = functionObj.get("FUNCTION_NAME").getAsString();
-            JsonObject thresholds = functionObj.getAsJsonObject("THRESHOLDS");
-            List<MetricDataQuery> metricQueries = new ArrayList<>();
-            int index = 0;
-            for (String metricName : thresholds.keySet()) {
-
-                MetricDataQuery query = MetricDataQuery.builder()
-                        .id("metric" + index)
-                        .metricStat(MetricStat.builder()
-                                .metric(Metric.builder()
-                                        .namespace("AWS/LambdaInsights")
-                                        .metricName(metricName)
-                                        .dimensions(Dimension.builder()
-                                                .name("FunctionName")
-                                                .value(functionName)
-                                                .build())
-                                        .build())
-                                .period(Integer.parseInt(timeRange) / 1000)
-                                .stat("Average")
-                                .build())
-                        .build();
-                metricQueries.add(query);
-                index++;
-            }
-            GetMetricDataRequest request = GetMetricDataRequest.builder()
-                    .startTime(Instant.ofEpochMilli(startTime))
-                    .endTime(Instant.now())
-                    .metricDataQueries(metricQueries)
-                    .build();
-            GetMetricDataResponse response = cw.getMetricData(request);
-            Map<String, Object> functionMetricsResponse = new HashMap<>();
-            for (MetricDataResult result : response.metricDataResults()) {
-                if (result.values().isEmpty()) {  // Check if metric data is empty
-                    functionMetricsResponse.put(result.label(), "Metric data not available");
-                    continue;
-                }
-                double actualValue = result.values().get(0);
-                String metricName = result.label();
-
-                double thresholdValue = thresholds.get(metricName).getAsDouble();
-                String status = (metricName.equals("memory_utilization") && actualValue < thresholdValue) ||
-                        (!metricName.equals("memory_utilization") && actualValue > thresholdValue) ?
-                        "DOWN" : "UP";
-
-                Map<String, Object> metricResult = new HashMap<>();
-                metricResult.put("REAL_VALUE", actualValue);
-                metricResult.put("THRESHOLD_VALUE", thresholdValue);
-                metricResult.put("STATUS", status);
-
-                functionMetricsResponse.put(metricName, metricResult);
-            }
-
-            if (functionMetricsResponse.isEmpty()) {
-                metricsResponse.put(functionName, "Function metrics not available or function not found");
-            } else {
-                metricsResponse.put(functionName, functionMetricsResponse);
-            }
-        }
-        return metricsResponse;
-    }
+//    public Map<String, Object> checkMetrics() {
+//        JsonObject configJson = gson.fromJson(configManager.get("HEALTH"), JsonObject.class);
+//        JsonObject metricsJson = configJson.getAsJsonObject("METRICS");
+//        String timeRange = metricsJson.get("TIME_RANGE").getAsString();
+//        long startTime = Instant.now().minusMillis(Long.parseLong(timeRange)).toEpochMilli();
+//
+//        CloudWatchClient cw = CloudWatchClient.builder().build();
+//
+//        Map<String, Object> metricsResponse = new HashMap<>();
+//        for (JsonElement functionElement : metricsJson.getAsJsonArray("FUNCTIONS")) {
+//            JsonObject functionObj = functionElement.getAsJsonObject();
+//            String functionName = functionObj.get("FUNCTION_NAME").getAsString();
+//            JsonObject thresholds = functionObj.getAsJsonObject("THRESHOLDS");
+//            List<MetricDataQuery> metricQueries = new ArrayList<>();
+//            int index = 0;
+//            for (String metricName : thresholds.keySet()) {
+//
+//                MetricDataQuery query = MetricDataQuery.builder()
+//                        .id("metric" + index)
+//                        .metricStat(MetricStat.builder()
+//                                .metric(Metric.builder()
+//                                        .namespace("AWS/LambdaInsights")
+//                                        .metricName(metricName)
+//                                        .dimensions(Dimension.builder()
+//                                                .name("FunctionName")
+//                                                .value(functionName)
+//                                                .build())
+//                                        .build())
+//                                .period(Integer.parseInt(timeRange) / 1000)
+//                                .stat("Average")
+//                                .build())
+//                        .build();
+//                metricQueries.add(query);
+//                index++;
+//            }
+//            GetMetricDataRequest request = GetMetricDataRequest.builder()
+//                    .startTime(Instant.ofEpochMilli(startTime))
+//                    .endTime(Instant.now())
+//                    .metricDataQueries(metricQueries)
+//                    .build();
+//            GetMetricDataResponse response = cw.getMetricData(request);
+//            Map<String, Object> functionMetricsResponse = new HashMap<>();
+//            for (MetricDataResult result : response.metricDataResults()) {
+//                if (result.values().isEmpty()) {  // Check if metric data is empty
+//                    functionMetricsResponse.put(result.label(), "Metric data not available");
+//                    continue;
+//                }
+//                double actualValue = result.values().get(0);
+//                String metricName = result.label();
+//
+//                double thresholdValue = thresholds.get(metricName).getAsDouble();
+//                String status = (metricName.equals("memory_utilization") && actualValue < thresholdValue) ||
+//                        (!metricName.equals("memory_utilization") && actualValue > thresholdValue) ?
+//                        "DOWN" : "UP";
+//
+//                Map<String, Object> metricResult = new HashMap<>();
+//                metricResult.put("REAL_VALUE", actualValue);
+//                metricResult.put("THRESHOLD_VALUE", thresholdValue);
+//                metricResult.put("STATUS", status);
+//
+//                functionMetricsResponse.put(metricName, metricResult);
+//            }
+//
+//            if (functionMetricsResponse.isEmpty()) {
+//                metricsResponse.put(functionName, "Function metrics not available or function not found");
+//            } else {
+//                metricsResponse.put(functionName, functionMetricsResponse);
+//            }
+//        }
+//        return metricsResponse;
+//    }
 }

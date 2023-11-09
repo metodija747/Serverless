7cf72cfc9d54acee2095234b4ffc994923e72dbc
Metodija747 <metodijabucevski@yahoo.com>
2023-11-09 18:56:01 +0100

    added healthchecks

--- src/main/java/serverless/CatalogProduct/AddNewProduct.java
+++ src/main/java/serverless/CatalogProduct/AddNewProduct.java
@@ -76,6 +76,7 @@ public class AddNewProduct implements RequestHandler<Map<String, Object>, Map<St
 
         Subsegment tokenVerificationSubsegment = AWSXRay.beginSubsegment("authenticatingUser");
         String token = ((Map<String, String>) event.get("headers")).get("Authorization");
+        System.out.println("OVA E TOKENOT: " + token);
         try {
             TokenVerifier.verifyToken(token, ISSUER);
             List<String> groups = TokenVerifier.getGroups(token, ISSUER);
